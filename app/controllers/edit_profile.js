17098612d0841e8e0543dfe857ad2d343050ef77
aniket-pracheta <aniket.pracheta@neosofttech.com>
2016-12-20 18:22:50 +0530

    picker of dob implementde

--- app/controllers/edit_profile.js
+++ app/controllers/edit_profile.js
@@ -11,7 +11,7 @@ var viewpicker = Titanium.UI.createView({
 	borderRadius : 10,
 	backgroundColor : '#283747',
 	width : "80%",
-	height : 450,
+	height :"80%",
 });
 
 var picker = Ti.UI.createPicker({
@@ -19,7 +19,7 @@ var picker = Ti.UI.createPicker({
 	dateTimeColor : "black",
 	//useSpinner for Android Only
 	//useSpinner : true,
-	minDate : new Date(2009, 0, 1),
+	minDate : new Date(1880, 0, 1),
 	maxDate : new Date(2030, 11, 31),
 	value : new Date(2014, 3, 12),
 	top : 0,
@@ -33,30 +33,32 @@ var set = Ti.UI.createButton({
 		borderRadius : 10
 	});
 
-///date-picker
-function getpicker() {
+//date-picker
+$.fourth.addEventListener('click', function() {
+	//alert("picker");
 	viewpicker.removeAllChildren();
 	viewpicker.add(picker);
 	viewpicker.add(set);
 	$.edit_profile_window.add(viewpicker);
 
 	
-}
+});
 set.addEventListener('click', function(e) {
 		var date = picker.value;
 		var day = date.getDate();
 		var month = date.getMonth() + 1;
 		var year = date.getFullYear();
 		var newdate = day + "-" + month + "-" + year;
-		alert(newdate);
+		//alert(newdate);
 		$.dob.setText(newdate);
+		//$.dob.value=newdate; 
 		$.dob.hintText=newdate;
 		$.edit_profile_window.remove(viewpicker);
 	});
 	
 var access_token=Alloy.Globals.user_data_fetch.data.access_token;
 function submit(){
-		
+		Ti.API.log(videoURL);
 		var formdata={
 			first_name:$.first_name.value,
 			last_name:$.last_name.value,
@@ -93,3 +95,28 @@ function submit(){
 				};
 	Alloy.Globals.someGlobalFunction(option_edit,sucesss,failure);
 }
+//######################### camera event ########################################
+var videoURL = null;
+$.user_image.addEventListener('click', function() {
+    // Start an activity with an intent to capture video
+    // http://developer.android.com/reference/android/provider/MediaStore.html#ACTION_VIDEO_CAPTURE
+    var intent = Titanium.Android.createIntent({ action: 'android.media.action.IMAGE_CAPTURE' });
+    $.edit_profile_window.activity.startActivityForResult(intent, function(e) {
+        if (e.resultCode == Ti.Android.RESULT_OK) {
+            if (e.intent.data != null) {
+                // If everything went OK, save a reference to the video URI
+                videoURL = e.intent.data;
+               // playButton.visible = true;
+    	    }
+            else {
+                Ti.API.error('Could not retrieve media URL!');
+            }
+        }
+        else if (e.resultCode == Ti.Android.RESULT_CANCELED) {
+            Ti.API.trace('User cancelled video capture session.');
+        }
+        else {
+            Ti.API.error('Could not record video!');
+        }
+    });
+});
